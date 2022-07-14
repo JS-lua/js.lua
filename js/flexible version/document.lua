d094c270e854e41b3cefd84e1d7f578aee618fc4
dudl1 <80582147+dudl1@users.noreply.github.com>
2022-07-14 15:28:12 +0700

    Update document.lua

--- js/flexible version/document.lua
+++ js/flexible version/document.lua
@@ -1,6 +1,6 @@
 local document = {}
 
-function document.querySelector(data: userdata)
+function document.querySelector(data)
 	
 	-- local sadsa = string.sub(data, 1, 20)
 	local a = workspace[data]
